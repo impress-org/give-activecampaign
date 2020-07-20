af1d5585e088d8052521acba051fdbefaf664d9f
Devin Walker <sdtucson@gmail.com>
2020-07-20 11:12:14 -0700

    add multiselect and API calls to pull lists and tags

--- includes/helpers.php
+++ includes/helpers.php
@@ -45,30 +45,73 @@ function give_get_activecampaign_lists() {
 }
 
 /**
+ * Pull tags into
+ *
+ * @return array
+ */
+function give_get_activecampaign_tags() {
+
+	$api_url = give_get_option( 'give_activecampaign_apiurl', false );
+	$api_key = give_get_option( 'give_activecampaign_api', false );
+
+	if ( ! $api_url || ! $api_key ) {
+		return array();
+	}
+
+	// Load ActiveCampaign API
+	if ( ! class_exists( 'ActiveCampaign' ) ) {
+		require_once( GIVE_ACTIVECAMPAIGN_PATH . '/vendor/ActiveCampaign.class.php' );
+	}
+
+	$ac = new ActiveCampaign( $api_url, $api_key );
+
+	$tags = $ac->api( 'tags/list', array( 'ids' => 'all' ) );
+
+	$tags = json_decode( $tags, true );
+
+	if ( ! empty( $tags ) ) {
+
+		$output = array();
+
+		foreach ( $tags as $key => $tag ) {
+			if ( ! is_numeric( $key ) ) {
+				continue;
+			}
+
+			$output[ $tag['id'] ] = $tag['name'];
+		}
+		error_log( print_r( $output, true ) . "\n", 3, WP_CONTENT_DIR . '/debug_new.log' );
+		return $output;
+	} else {
+		return array();
+	}
+
+}
+
+/**
  * Display the opt-in checkbox oon donation forms.
  */
 function give_activecampaign_display_optin( $form_id ) {
 
 	$label = give_get_option( 'give_activecampaign_label' );
 
-	if( ! empty( $label ) ) {
+	if ( ! empty( $label ) ) {
 		$checkout_label = trim( $label );
 	} else {
 		$checkout_label = __( 'Subscribe to our newsletter', 'edd-activecampaign' );
 	}
 
 	// Should the opt-on be checked or unchecked by default?
-	$form_option    = give_get_meta( $form_id, '_give_mailchimp_checked_default', true );
+	$form_option = give_get_meta( $form_id, '_give_mailchimp_checked_default', true );
 
 	$checked = give_get_option( 'give_activecampaign_checkbox_default', false );
 
-
-
 	ob_start(); ?>
-	<fieldset id="give_activecampaign<?php echo $form_id; ?>" class="give-mailchimp-fieldset">
-		<label for="give_activecampaign<?php echo $form_id; ?>_signup" class="give-activecampaign-optin-label">
-			<input name="give_activecampaign<?php echo $form_id; ?>_signup"
-			       id="give_activecampaign<?php echo $form_id; ?>_signup"
+	<fieldset id="give_activecampaign_<?php echo $form_id; ?>" class="give-activecampaign-fieldset">
+		<label for="give_activecampaign_<?php echo $form_id; ?>_signup" class="give-activecampaign-optin-label">
+			<input name="give_activecampaign_<?php echo $form_id; ?>_signup"
+			       class="give-activecampaign-optin-input"
+			       id="give_activecampaign_<?php echo $form_id; ?>_signup"
 			       type="checkbox" <?php echo( $checked !== 'no' ? 'checked="checked"' : '' ); ?>/>
 			<span class="give-activecampaign-message-text"><?php echo $checkout_label; ?></span>
 		</label>
@@ -80,3 +123,46 @@ function give_activecampaign_display_optin( $form_id ) {
 
 
 add_action( 'give_donation_form_before_submit', 'give_activecampaign_display_optin', 10, 1 );
+
+
+/**
+ * Add an email address to the ActiveCampaign list.
+ *
+ * @access public
+ *
+ * @param string $email      Email address.
+ * @param string $first_name First name.
+ * @param string $last_name  Last name.
+ * @param int    $list       List ID.
+ *
+ * @return bool
+ * @since  1.0
+ *
+ */
+function give_activecampaign_subscribe_email( $email, $first_name = '', $last_name = '', $list = 0 ) {
+
+	$api_url = give_get_option( 'give_activecampaign_apiurl', false );
+	$api_key = give_get_option( 'give_activecampaign_api', false );
+
+	if ( $api_key ) {
+
+		// Load ActiveCampaign API.
+		if ( ! class_exists( 'ActiveCampaign' ) ) {
+			require_once( 'vendor/ActiveCampaign.class.php' );
+		}
+
+		$ac = new ActiveCampaign( $api_key, $api_url );
+
+		$subscriber = array(
+			"email"           => "$email",
+			"first_name"      => "$first_name",
+			"last_name"       => "$last_name",
+			"p[{$list}]"      => $list,
+			"status[{$list}]" => 1,
+		);
+
+		$ac->api( "contact/add", $subscriber );
+	}
+
+	return false;
+}
