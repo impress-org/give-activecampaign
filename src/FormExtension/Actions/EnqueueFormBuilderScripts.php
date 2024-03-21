0a7a901c7bdb0176715654f5b967f55c825a301d
Joshua Dinh <josh@joshuadinh.tech>
2024-03-20 17:45:40 -0700

    refactor: use credential test prior to api calls, add method for tags, update localized script.

--- src/FormExtension/Actions/EnqueueFormBuilderScripts.php
+++ src/FormExtension/Actions/EnqueueFormBuilderScripts.php
@@ -2,31 +2,33 @@
 
 namespace GiveActiveCampaign\FormExtension\Actions;
 
+use Give\Log\Log;
+
 class EnqueueFormBuilderScripts
 {
     /**
      * @var \ActiveCampaign
      */
     protected $activecampaign;
-
+    
     /**
      * @unreleased
      * @var string
      */
     protected $styleSrc;
-
+    
     /**
      * @unreleased
      * @var string
      */
     protected $scriptSrc;
-
+    
     /**
      * @unreleased
      * @var array
      */
     protected $scriptAsset;
-
+    
     /**
      * @unreleased
      */
@@ -37,33 +39,50 @@ class EnqueueFormBuilderScripts
         $this->scriptSrc = GIVE_ACTIVECAMPAIGN_URL . 'build/FormBuilder.js';
         $this->scriptAsset = require GIVE_ACTIVECAMPAIGN_DIR . 'build/FormBuilder.asset.php';
     }
-
+    
     /**
      * @unreleased
      */
     public function __invoke()
     {
-        $tags = json_decode($this->activecampaign->api( 'tags/list', [ 'ids' => 'all' ] ));
-
         wp_enqueue_script('givewp-form-extension-activecampaign', $this->scriptSrc, $this->scriptAsset['dependencies']);
+        
         wp_localize_script('givewp-form-extension-activecampaign', 'GiveActiveCampaign', [
-            'lists' => $this->getLists(),
-            'tags' => array_map(function ($tag) {
-                return ['value' => $tag->name, 'label' => $tag->name];
-            }, $tags),
+            'requiresSetup' => ! $this->activecampaign->credentials_test(),
+            'settingsUrl'   =>  '/wp-admin/edit.php?post_type=give_forms&page=give-settings&tab=activecampaign',
+            'lists' =>  $this->getLists(),
+            'tags'  => $this->getTags(),
         ]);
+        
+        wp_enqueue_style('givewp-form-extension-active-campaign', $this->styleSrc);
     }
-
+    
     protected function getLists(): array
     {
-        $lists = (array) $this->activecampaign->api( 'list/list', [ 'ids' => 'all' ] );
-
-        $lists = array_filter($lists, function ($list) {
-            return $list->name;
-        });
-
-        return array_map(function ($list) {
-            return ['value' => $list->id, 'label' => $list->name];
-        }, $lists );
+        if ($this->activecampaign->credentials_test()) {
+            $lists = (array)$this->activecampaign->api('list/list', ['ids' => 'all']);
+            
+            $lists = array_filter($lists, function ($list) {
+                return $list->name;
+            });
+            
+            return array_map(function ($list) {
+                return ['value' => $list->id, 'label' => $list->name];
+            }, $lists);
+        }
+        
+        return [];
+    }
+    
+    protected function getTags(): array
+    {
+        if ($this->activecampaign->credentials_test()) {
+            $tags = json_decode($this->activecampaign->api('tags/list', ['ids' => 'all']));
+            return array_map(function ($tag) {
+                return ['value' => $tag->name, 'label' => $tag->name];
+            }, $tags);
+        }
+        
+        return [];
     }
 }
