85fa7273683e2c5e1cb29529f887f3945db3b309
Joshua Dinh <josh@joshuadinh.tech>
2024-03-20 17:40:23 -0700

    feature: add block placeholder and notice

---
+++ src/FormExtension/FormBuilder/Block/BlockPlaceholder/index.tsx
@@ -0,0 +1,40 @@
+import { CheckboxControl } from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
+import "./styles.scss";
+import { getWindowData } from "../window";
+import { createInterpolateElement } from "@wordpress/element";
+
+/**
+ * @since 2.0.0
+ */
+export default function BlockPlaceholder({ defaultChecked, label }) {
+  const { requiresSetup, settingsUrl } = getWindowData();
+
+  return (
+    <div
+      className={`givewp-active-campaign-block-placeholder
+			${requiresSetup && "givewp-active-campaign-block-placeholder--invalid"}`}
+    >
+      {requiresSetup ? (
+        createInterpolateElement(
+          __(
+            "This block requires additional setup. Go to your <a>Settings</a> to connect your Active Campaign account.",
+            "give"
+          ),
+          {
+            a: (
+              <a href={settingsUrl} target="_blank" rel="noopener noreferrer" />
+            ),
+          }
+        )
+      ) : (
+        <CheckboxControl
+          checked={defaultChecked}
+          label={label}
+          onChange={null}
+          disabled={true}
+        />
+      )}
+    </div>
+  );
+}
