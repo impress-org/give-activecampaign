90055d4160fecdbe98b710093a70557865f0c7bd
Joshua Dinh <josh@joshuadinh.tech>
2024-03-20 17:47:47 -0700

    feature: add credential test prior to adding new block

--- src/FormExtension/Actions/RenderDonationFormBlock.php
+++ src/FormExtension/Actions/RenderDonationFormBlock.php
@@ -20,36 +20,42 @@ class RenderDonationFormBlock
      * @return ActiveCampaignField
      * @throws EmptyNameException
      */
-    public function __invoke($node, BlockModel $block, int $blockIndex): ActiveCampaignField
+    public function __invoke($node, BlockModel $block, int $blockIndex):? ActiveCampaignField
     {
-        return ActiveCampaignField::make('activecampaign')
-            ->label((string)$block->getAttribute('label'))
-            ->checked((bool)$block->getAttribute('defaultChecked'))
-            ->selectedLists((array)$block->getAttribute('selectedLists'))
-            ->selectedTags((array)$block->getAttribute('selectedTags'))
-            ->scope(function(ActiveCampaignField $field, $value, Donation $donation) {
-
-                // If the field is checked, subscribe the donor to the list.
-                if(filter_var($value, FILTER_VALIDATE_BOOLEAN)) {
-
-                    $subscriber = [
-                        "email"      => $donation->donor->email,
-                        "first_name" => $donation->donor->firstName,
-                        "last_name"  => $donation->donor->lastName,
-                        "tags"       => implode( ', ', $field->getSelectedTags() ),
-                    ];
-
-                    foreach ( $field->getSelectedLists() as $list ) {
-                        $subscriber["p[$list]"] = $list;
+        $activeCampaign = give(\ActiveCampaign::class);
+        
+        if ($activeCampaign->credentials_test()) {
+            return ActiveCampaignField::make('activecampaign')
+                ->label((string)$block->getAttribute('label'))
+                ->checked((bool)$block->getAttribute('defaultChecked'))
+                ->selectedLists((array)$block->getAttribute('selectedLists'))
+                ->selectedTags((array)$block->getAttribute('selectedTags'))
+                ->scope(function(ActiveCampaignField $field, $value, Donation $donation) {
+    
+                    // If the field is checked, subscribe the donor to the list.
+                    if(filter_var($value, FILTER_VALIDATE_BOOLEAN)) {
+    
+                        $subscriber = [
+                            "email"      => $donation->donor->email,
+                            "first_name" => $donation->donor->firstName,
+                            "last_name"  => $donation->donor->lastName,
+                            "tags"       => implode( ', ', $field->getSelectedTags() ),
+                        ];
+    
+                        foreach ( $field->getSelectedLists() as $list ) {
+                            $subscriber["p[$list]"] = $list;
+                        }
+    
+                        $response = give(\ActiveCampaign::class)->api( "contact/add", $subscriber );
+    
+                        // Add meta to the donation post that this donation opted-in to ActiveCampaign.
+                        if ( ! empty( $field->getSelectedTags() ) || ! empty( $field->getSelectedLists() ) ) {
+                            add_post_meta( $donation->id, '_give_activecampaign_donation_optin_status', 'true' );
+                        }
                     }
-
-                    $response = give(\ActiveCampaign::class)->api( "contact/add", $subscriber );
-
-                    // Add meta to the donation post that this donation opted-in to ActiveCampaign.
-                    if ( ! empty( $field->getSelectedTags() ) || ! empty( $field->getSelectedLists() ) ) {
-                        add_post_meta( $donation->id, '_give_activecampaign_donation_optin_status', 'true' );
-                    }
-                }
-            });
+                });
+            }
+        
+        return null;
     }
 }
