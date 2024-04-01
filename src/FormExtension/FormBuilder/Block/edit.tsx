f7556c553524bfba942f4f09633193e34ec071a9
Joshua Dinh <josh@joshuadinh.tech>
2024-04-01 12:32:08 -0700

    refactor: use update inspectorControls

--- src/FormExtension/FormBuilder/Block/edit.tsx
+++ src/FormExtension/FormBuilder/Block/edit.tsx
@@ -1,17 +1,6 @@
-import { __ } from '@wordpress/i18n';
-import { BlockEditProps } from '@wordpress/blocks';
-import { InspectorControls } from '@wordpress/block-editor';
-import {
-  PanelBody,
-  SelectControl,
-  TextControl,
-  ToggleControl,
-} from "@wordpress/components";
+import {BlockEditProps} from "@wordpress/blocks";
 import BlockPlaceholder from "./BlockPlaceholder";
-import { getWindowData } from "./window";
-import { BlockNotice } from "@givewp/form-builder-library";
-
-const { lists, tags } = getWindowData();
+import BlockInspectorControls from "./BlockInspectorControls";
 
 /**
  * @unreleased
@@ -20,70 +9,12 @@ export default function Edit({
   attributes,
   setAttributes,
 }: BlockEditProps<any>) {
-  const { defaultChecked, label, selectedLists, selectedTags } = attributes;
-  const { settingsUrl, requiresSetup } = getWindowData();
+  const { defaultChecked, label } = attributes;
 
   return (
     <>
       <BlockPlaceholder {...{ defaultChecked, label }} />
-      <InspectorControls>
-        <PanelBody
-          title={__('Field Settings', 'give-activecampaign')}
-          initialOpen={true}
-        >
-          {requiresSetup ? (
-            <BlockNotice
-              title={__('ActiveCampaign requires setup', 'give')}
-              description={__(
-                'This block requires your settings to be configured in order to use.',
-                'give'
-              )}
-              anchorText={__('Connect your ActiveCampaign account', 'give')}
-              href={settingsUrl}
-            />
-          ) : (
-            <div className={'givewp-activecampaign-controls'}>
-              <TextControl
-                label={__('Custom Label', 'give-activecampaign')}
-                value={label}
-                help={__(
-                  'Customize the label for the ActiveCampaign opt-in checkbox',
-                  'give-activecampaign'
-                )}
-                onChange={(value) => setAttributes({ label: value })}
-              />
-
-              <ToggleControl
-                label={__('Opt-in Default', 'give-activecampaign')}
-                checked={defaultChecked}
-                onChange={() =>
-                  setAttributes({ defaultChecked: !defaultChecked })
-                }
-                help={__(
-                  'Customize the newsletter opt-in option for this form.',
-                  'give-activecampaign'
-                )}
-              />
-
-              <SelectControl
-                multiple={true}
-                label={__('Lists', 'give-activecampaign')}
-                value={selectedLists}
-                onChange={(value) => setAttributes({ selectedLists: value })}
-                options={lists}
-              />
-
-              <SelectControl
-                multiple={true}
-                label={__('Tags', 'give-activecampaign')}
-                value={selectedTags}
-                onChange={(value) => setAttributes({ selectedTags: value })}
-                options={tags}
-              />
-            </div>
-          )}
-        </PanelBody>
-      </InspectorControls>
+      <BlockInspectorControls {...{ attributes, setAttributes }} />
     </>
   );
 }
