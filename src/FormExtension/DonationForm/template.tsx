2e870db60411f21d7299f31a2d836a63ff25b878
Joshua Dinh <josh@joshuadinh.tech>
2024-04-02 09:00:06 -0700

    chore: formatting

--- src/FormExtension/DonationForm/template.tsx
+++ src/FormExtension/DonationForm/template.tsx
@@ -6,22 +6,16 @@ const { checkbox: Checkbox } = window.givewp.form.templates.fields;
 /**
  * @unreleased
  */
-export default function FieldTemplate({
-  ErrorMessage,
-  label,
-  defaultChecked,
-  inputProps,
-}) {
-  return (
-    <div className={"givewp-activecampaign-field"}>
-      <Checkbox
-        Label={() => label}
-        ErrorMessage={ErrorMessage}
-        inputProps={{
-          defaultChecked: defaultChecked,
-          ...inputProps,
-        }}
-      />
-    </div>
-  );
-}
+export default function FieldTemplate({ErrorMessage, label, defaultChecked, inputProps}) {
+    return (
+        <div className={'givewp-activecampaign-field'}>
+            <Checkbox
+                Label={() => label}
+                ErrorMessage={ErrorMessage}
+                inputProps={{
+                    defaultChecked: defaultChecked,
+                    ...inputProps,
+                }}
+            />
+        </div>
+    );
